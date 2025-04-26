f47dffe69fa44b89c57a3148f21ef284eb209171
laytonmusyoki <laytonmatheka@gmail.com>
2025-04-26 18:17:37 +0300

    modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -7,7 +7,7 @@ function Modal({isOpen,onClose,children}) {
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 p-2 z-50">
         <div className="bg-white p-3 rounded-lg w-full max-w-md">
         <div className="flex justify-end">
-            <FaTimes onClick={onClose}/>
+            <FaTimes size={20} onClick={onClose}/>
         </div>
         {children}
         </div>
