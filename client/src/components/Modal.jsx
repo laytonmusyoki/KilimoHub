252a429f4fcbec67526feaea12db75a6791914e5
laytonmusyoki <laytonmatheka@gmail.com>
2025-04-26 18:11:31 +0300

    modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -4,7 +4,7 @@ function Modal({isOpen,onClose,children}) {
     if(!isOpen) return null
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 p-2 z-50">
-        <div className="bg-white p-3 rounded-lg w-96">
+        <div className="bg-white p-3 rounded-lg max-w-md">
         <div className="flex justify-end">
             <button onClick={onClose} className="text-black text-2xl">×</button>
         </div>
